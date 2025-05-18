746a505e714270745a6fc47d22437a1958431167
ectsy <johnroy.tja@gmail.com>
2025-05-18 14:12:14 -0400

    Detects if :8080 is occupied

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
 	"net/http"
 	"github.com/gorilla/mux"
 	"restapi.com/app/routes"
@@ -9,14 +11,30 @@ import (
 	"restapi.com/app/database"
 )
 
+func isPortAvailable(port string)bool{
+	listener, err := net.Listen("tcp", port)
+	if err != nil{
+		return false
+	}
+	_ = listener.Close()
+	return true
+}
 
 func main(){
+	port := ":8080"
+
+	if !isPortAvailable(port){
+		log.Fatalf("Port %s is already in use. Please close the conflicting application and run again", port)
+	}
+
 	database.Connect()
 	database.DB.AutoMigrate(&models.User{})
 
 	router := mux.NewRouter()
 	routes.RegisterRoutes(router)
 
-	fmt.Println("Server running at http://localhost:8080")
-	http.ListenAndServe(":8080", router)
+	fmt.Println("Server running at http://localhost%s\n", port)
+	if err := http.ListenAndServe(port, router); err != nil{
+		log.Fatal(err)
+	}
 }
